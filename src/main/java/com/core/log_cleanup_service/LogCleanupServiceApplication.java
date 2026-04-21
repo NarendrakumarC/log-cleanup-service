9d542f9025fbcf80dd29cd1527dff4d4c83cedfc
narendrakumarc <narendrakumarc500@gmail.com>
2026-04-21 21:27:40 +0530

    project setup and initialization

---
+++ src/main/java/com/core/log_cleanup_service/LogCleanupServiceApplication.java
@@ -0,0 +1,13 @@
+package com.core.log_cleanup_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogCleanupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogCleanupServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/core/log_cleanup_service/LogCleanupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.log_cleanup_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogCleanupServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
